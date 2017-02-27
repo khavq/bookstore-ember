c8b479c33d027303fbe55d7cefb316dca291317c
khavq <khait90@gmail.com>
2017-02-27 12:30:37 -0800

    delete node_modules

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
-const { computed } = Ember;
+// const { computed } = Ember;
+// import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
-	queryParams: ['limit'],
-	limit: 5,
+	// queryParams: ['limit'],
+	// limit: 5,
 
-	total: computed('model.meta', function(){
-		return this.get('model.meta').total;
-	}),
+	// total: computed('model.meta', function(){
+	// 	return this.get('model.meta').total;
+	// }),
 
-	showAll: computed('total', 'model', function() {
-		return this.get('total') > this.get('model.length');
-	})
+	// showAll: computed('total', 'model', function() {
+	// 	return this.get('total') > this.get('model.length');
+	// })
 });
 
 
--- app/routes/book.js
+++ app/routes/book.js
@@ -1,24 +1,28 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import InfinityRoute from "ember-infinity/mixins/route";
+// import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
+
 
 export default Ember.Route.extend(InfinityRoute, AuthenticatedRouteMixin, {
-	queryParams: {
-		limit: {
-			refreshModel: true
-		}
-	},
+	// queryParams: {
+	// 	limit: {
+	// 		refreshModel: true
+	// 	}
+	// },
+	totalPagesParam: "meta.total-pages",    // instead of "meta.total_pages"
 
-	model() {
+	model(params) {
 		// console.log(params);
 		// return this.store.query('book', params);
-		return this.infinityModel("book");
-	},
-
-	actions: {
-		showAll() {
-			const total = this.controllerFor('books').get('total');
-			this.transitionTo({ queryParams: {limit: total}});
-		}
+		return this.infinityModel("book", { perPage: 12, startingPage: 1 });
+		// return this.findPaged('book', params);
 	}
+
+	// actions: {
+	// 	showAll() {
+	// 		const total = this.controllerFor('books').get('total');
+	// 		this.transitionTo({ queryParams: {limit: total}});
+	// 	}
+	// }
 });
