5cbe9c69c41f2af54ff0a8f60bdb62eac9dae8b7
khavq <khait90@gmail.com>
2017-03-01 18:31:44 -0800

    search

--- app/controllers/books.js
+++ app/controllers/books.js
@@ -1,33 +1,19 @@
 import Ember from 'ember';
 // const { computed } = Ember;
-// import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Controller.extend({
-	// queryParams: ['limit'],
-	// limit: 5,
+	queryParams: ['search'],
+  	search: 'anyway',
 
-	// total: computed('model.meta', function(){
-	// 	return this.get('model.meta').total;
-	// }),
+  	filteredArticles: Ember.computed('search', 'model', function() {
+	    var search = this.get('search');
+	    // return category;
+	    var books = this.get('model');
 
-	// showAll: computed('total', 'model', function() {
-	// 	return this.get('total') > this.get('model.length');
-	// })
-	handleFilterEntry: computed(function(){
-
-	}),
-
-	actions: {
-	    filterByCity(param) {
-	      if (param !== '') {
-	        return this.get('store').query('rental', { city: param });
-	      } else {
-	        return this.get('store').findAll('rental');
-	      }
+	    if (search) {
+	      return books.filterBy('title', search);
+	    } else {
+	      return books;
 	    }
-	  }
-
+  	})
 });
-
-
-
