de2876b317639a19187e56b122b8e12a862c9546
khavq <khait90@gmail.com>
2017-03-01 18:31:29 -0800

    search

--- app/routes/book.js
+++ app/routes/book.js
@@ -5,25 +5,47 @@ import InfinityRoute from "ember-infinity/mixins/route";
 
 
 export default Ember.Route.extend(InfinityRoute, AuthenticatedRouteMixin, {
-	// queryParams: {
-	// 	limit: {
-	// 		refreshModel: true
-	// 	}
-	// },
+	queryParams: {
+		limit: {
+			refreshModel: true
+		},
+		search: {
+			refreshModel: true
+		}
+	},
 	totalPagesParam: "meta.total-pages",    // instead of "meta.total_pages"
 
 	model(params) {
 		// console.log(params);
 		// return this.store.query('book', params);
 		//var search = this.get('txt_search');
-		return this.infinityModel("book", { perPage: 12, startingPage: 1, search: "" });
+		return this.infinityModel("book", params);
 		// return this.findPaged('book', params);
 	},
 
 	actions: {
+		// willTransition(transition) {
+	 //      if (!confirm('Are you sure you want to abandon progress?')) {
+	 //        transition.abort();
+	 //      } else {
+	 //        // Bubble the `willTransition` action so that
+	 //        // parent routes can decide whether or not to abort.
+	 //        console.log("xxx");
+	 //        return true;
+	 //      }
+	 //    },
+	    
 		showAll() {
 			// const total = this.controllerFor('books').get('total');
 			// this.transitionTo({ queryParams: {limit: total}});
+		},
+
+		filteredArticles() {
+
+			const search = this.controllerFor('books').get('search');
+			console.log("search:",search);
+			// return this.infinityModel("book", { perPage: 12, startingPage: 1, search: "" });
+			this.transitionTo({ queryParams: {search: search}});
 		},
 
 		handleFilterEntry() {
