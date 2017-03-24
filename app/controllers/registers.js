ea3e43e32df3e3b1a954ba482571de20286bd042
khavq <khait90@gmail.com>
2017-03-24 16:25:21 -0700

    get current user

--- app/controllers/registers.js
+++ app/controllers/registers.js
@@ -1,61 +1,44 @@
 import Ember from 'ember';
-import EmberValidations from 'ember-validations';
+// import EmberValidations from 'ember-validations';
 
-export default Ember.Controller.extend(EmberValidations, {
-    showErrors: false,
+export default Ember.Controller.extend({
+  // showErrors: false,
+  // session: Ember.inject.service(),
+  // validations: {
+  //   userName: {
+  //     presence: true
+  //   },
+  //   password: {
+  //     presence: true,
+  //     length: {minimum: 6}
+  //   }
+  // },
+  // actions: {
+  //   register(){
+  //     let {userName, password} = this.getProperties('userName', 'password');
+  //     this.validate().then(()=>{
+  //       return this.get("session").register(userName, password)
+  //     }).then(()=>{
+  //         this.get('flashMessages').success('You have signed up successfully')
+  //         this.transitionToPreviousRoute()
+  //     }).catch((reason)=>{
+  //       this.set("showErrors", true)
+  //       if(typeof(reason) === 'string'){
+  //         this.get('flashMessages').danger(reason, {sticky: true})
+  //       }
+  //     })
+  //   }
+  // },
 
-    session: Ember.inject.service(),
-
-    validations: {
-        userName: {
-            presence: true
-        },
-        password: {
-            presence: true,
-            length: { minimum: 6 }
-        }
-    },
-
-    actions: {
-        register: function(){
-            let { userName, password } = this.getProperties('userName', 'password');
-            this.validate().then(() => {
-                return this.get("session").register(userName, password)
-            }).then(() => {
-                this.get('flashMessages').success('You have signed up successfully')
-                this.transitionToPreviousRoute()
-            }).catch((reason) => {
-                this.set("showErrors", true)
-                if (typeof(reason) === 'string') {
-                    this.get('flashMessages').danger(reason, { sticky: true })
-                }
-            })
-        }
-        // register() {
-        //     let { userName, password } = this.getProperties('userName', 'password');
-        //     this.validate().then(() => {
-        //         return this.get("session").register(userName, password)
-        //     }).then(() => {
-        //         this.get('flashMessages').success('You have signed up successfully')
-        //         this.transitionToPreviousRoute()
-        //     }).catch((reason) => {
-        //         this.set("showErrors", true)
-        //         if (typeof(reason) === 'string') {
-        //             this.get('flashMessages').danger(reason, { sticky: true })
-        //         }
-        //     })
-        // }
-    },
-
-    transitionToPreviousRoute() {
-        var previousTransition = this.get('previousTransition');
-        if (previousTransition) {
-            this.set('previousTransition', null);
-            previousTransition.retry();
-        } else {
-            // Default back to homepage
-            this.transitionToRoute('index');
-        }
-    }
+  // transitionToPreviousRoute(){
+  //   var previousTransition = this.get('previousTransition');
+  //   if (previousTransition) {
+  //     this.set('previousTransition', null);
+  //     previousTransition.retry();
+  //   } else {
+  //     // Default back to homepage
+  //     this.transitionToRoute('index');
+  //   }
+  // }
 
 })
--- app/services/session.js
+++ app/services/session.js
@@ -3,15 +3,6 @@ import ESASession from "ember-simple-auth/services/session";
 
 export default ESASession.extend({
 
-    store: Ember.inject.service(),
-
-    currentUser: Ember.computed('isAuthenticated', function() {
-        if (this.get('isAuthenticated')) {
-            const promise = this.get('store').queryRecord('member', {})
-            return DS.PromiseObject.create({ promise: promise })
-        }
-    }),
-
     actions: {
 
         register(userName, password) {
