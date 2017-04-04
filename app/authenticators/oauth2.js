1830163d88343dd546edcc7b8a2fa7bdfd0ef5f0
khavq <khait90@gmail.com>
2017-04-04 15:28:33 -0700

    proxy to AWS

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: "http://localhost:3000/oauth/token"
+  serverTokenEndpoint: "http://ec2-52-42-253-200.us-west-2.compute.amazonaws.com/oauth/token"
 });
