fa6c62f406e08585acd0a70572e263b4182b4c2e
khavq <khait90@gmail.com>
2017-04-05 17:46:26 -0700

    add ENV.host

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
-import ENV from '../../config/emvironment';
+// import ENV from 'bookstore/config/emvironment';
 
 const { String: { pluralize, underscore } } = Ember;
 
@@ -10,7 +10,8 @@ export default JSONAPIAdapter.extend(DataAdapterMixin, {
 	// namespace: 'api',
 	authorizer: 'authorizer:application',
 	
-	host: ENV.host,
+	// host: ENV.host,
+	host: 'http://ec2-52-42-253-200.us-west-2.compute.amazonaws.com/api/v1',
 
 	pathForType(type) {
 		return pluralize(underscore(type));
