72f187e64f4c6fcd7187c3f80a5c99c58efaff96
khavq <khait90@gmail.com>
2017-04-18 17:40:43 -0700

    material design

--- app/models/book.js
+++ app/models/book.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   price: DS.attr('number'),
+  picture1: DS.attr('string'),
   author: DS.belongsTo('author', { inverse: 'books' }),
   publisher: DS.belongsTo('publisher', { polymorphic: true, inverse: 'published' })
 });
