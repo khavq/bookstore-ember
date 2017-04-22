b328891c77ed5d47c073057bfb8061e63bfcbebc
khavq <khait90@gmail.com>
2017-04-21 18:10:24 -0700

    cart

--- app/models/member.js
+++ app/models/member.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  email: DS.attr('string')
+  email: DS.attr('string'),
+  full_name: DS.attr('string'),
+  avatar_url: DS.attr('string')
 });
